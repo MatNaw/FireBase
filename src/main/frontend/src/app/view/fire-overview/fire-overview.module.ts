1fe970d57dadd468ff8a8b59a52ea821739dc64f
PatrycjaKarbownik <patrycja@karbownik.net>
2020-04-18 19:50:06 +0200

    refactor

--- src/main/frontend/src/app/view/fire-overview/fire-overview.module.ts
+++ src/main/frontend/src/app/view/fire-overview/fire-overview.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
+import { environment } from '@env/environment';
 import { SharedModule } from '@app/shared/shared.module';
 import { FireOverviewComponent } from '@app/view/fire-overview/fire-overview.component';
 import { FireOverviewRoutingModule } from '@app/view/fire-overview/fire-overview-routing.module';
 import { ReportFireComponent } from '@app/view/fire-overview/report-fire/report-fire.component';
-import { AgmCoreModule } from '@agm/core';
-import { environment } from '@env/environment';
 
 @NgModule({
   declarations: [
