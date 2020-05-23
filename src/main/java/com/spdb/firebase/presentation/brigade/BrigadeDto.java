75a7db97ba8835f70cfd8a1e65faf0ee841eca3a
PatrycjaKarbownik <patrycja@karbownik.net>
2020-05-23 14:59:43 +0200

    refactor: remove mappers

--- src/main/java/com/spdb/firebase/presentation/brigade/BrigadeDto.java
+++ src/main/java/com/spdb/firebase/presentation/brigade/BrigadeDto.java
@@ -1,5 +1,6 @@
 package com.spdb.firebase.presentation.brigade;
 
+import com.spdb.firebase.domain.brigade.Brigade;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +15,26 @@ public class BrigadeDto {
     private String postalCode;
     private String street;
     private Long squadMaxAmount;
+
+    public static Brigade toBrigade(BrigadeDto brigadeDto) {
+        return Brigade.builder()
+                .id(brigadeDto.getId())
+                .name(brigadeDto.getName())
+                .city(brigadeDto.getCity())
+                .postalCode(brigadeDto.getPostalCode())
+                .street(brigadeDto.getStreet())
+                .squadMaxAmount(brigadeDto.getSquadMaxAmount())
+                .build();
+    }
+
+    public static BrigadeDto fromBrigade(Brigade brigade) {
+        return BrigadeDto.builder()
+                .id(brigade.getId())
+                .name(brigade.getName())
+                .city(brigade.getCity())
+                .postalCode(brigade.getPostalCode())
+                .street(brigade.getStreet())
+                .squadMaxAmount(brigade.getSquadMaxAmount())
+                .build();
+    }
 }
--- src/main/java/com/spdb/firebase/presentation/fire/FireDto.java
+++ src/main/java/com/spdb/firebase/presentation/fire/FireDto.java
@@ -1,13 +1,16 @@
 package com.spdb.firebase.presentation.fire;
 
+import com.spdb.firebase.domain.fire.Fire;
 import com.spdb.firebase.domain.fire.Squad;
 import com.spdb.firebase.domain.fire.Status;
+import com.spdb.firebase.presentation.brigade.BrigadeDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
@@ -20,4 +23,18 @@ public class FireDto {
     private LocalDate date;
     private Status status;
     private List<SquadDto> squads;
+
+    public static FireDto fromFire(Fire fire) {
+        return FireDto.builder()
+                .id(fire.getId())
+                .date(fire.getDate())
+                .postalCode(fire.getPostalCode())
+                .city(fire.getCity())
+                .street(fire.getStreet())
+                .status(fire.getStatus())
+                .squads(fire.getSquads().stream()
+                        .map(SquadDto::fromSquad)
+                        .collect(Collectors.toList()))
+                .build();
+    }
 }
